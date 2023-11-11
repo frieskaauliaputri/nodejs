3ff3a291d4771d39e8a3ebd8d1a24d9767e57834
frieskaauliaputri <frieskaaulia08@gmail.com>
2023-11-11 13:48:42 +0700

    nodejs

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const mysql = require('mysql2')
+const bookRoute = require('./routes/book')
+const authorRoute = require('./routes/author')
+const authRoute = require('./routes/auth')
+const dbConfig = require('./config/database')
+const pool = mysql.createPool(dbConfig)
+const authenticateJWT = require('./middleware/auth')
+const cors = require('cors')
+
+pool.on('error', (err) => {
+    console.log(err);
+})
+
+const app = express()
+const PORT = 3000
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({
+    extended: true
+}))
+
+// membuat parameter harus diawali dengan ( : )
+app.get('/contohparameter/:username/:jurusan/:kelas', (req, res) => {
+    res.json(req.params)
+})
+
+app.get('/contohparam', (req, res) => {
+    res.json(req.query)
+})
+
+app.get('/', (req, res) => {
+    res.write('hello world')
+    res.end()
+
+//     koneksi.connect(function(err){
+//     if(err) {
+//         console.log('Database connection failed: ' + err);
+//     } else {
+        
+//     }
+// })
+})
+
+app.use('/auth', authRoute)
+app.use('/book', authenticateJWT ,bookRoute)
+app.use('/author', authorRoute)
+
+app.listen(PORT, () => {
+    console.log(`server berjalan di http://localhost:${PORT}`);
+})
+
---
+++ routes/book.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const router = express.Router()
+const {
+    getBooks,
+    getBook,
+    addBook,
+    updateBook,
+    deleteBook,
+    search,
+    sortBy
+} = require('../controllers/BookController')
+//contoh akses http://localhost:3000/search?keyword=mau cari apa
+router.get('/search', search)
+//contoh akses http://localhost:3000/search?order=DESC / ASC 
+router.get('/sort',sortBy)
+// route untuk menampilkan data 
+router.get('/', getBooks)
+
+// route untuk mengirim data    
+router.post('/', addBook)
+
+//router untuk menampilkan data berdasarkan id buku 
+router.get('/:id', getBook)
+
+// route untuk memperbarui / update data
+router.put('/:id', updateBook)
+
+// route untuk menghapus data
+router.delete('/:id',deleteBook)
+
+module.exports = router
